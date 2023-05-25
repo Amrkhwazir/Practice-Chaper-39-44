85bab6866fbbef8f1e0ab5fc370a2f81863823af
Amirkhan567 <amrkh5678@gmail.com>
2023-05-25 15:51:57 +0500

    practice of chapter 39 to 44

---
+++ app.js
@@ -0,0 +1,171 @@
+// Switch Statments and while and Do while loop
+
+//Qno: 01
+
+// let a = prompt('enter number');
+
+// switch(true) {
+
+// case a>18:
+// console.log('old enough');
+//     break;
+
+// case a<18:
+// console.log('Too young');
+//     break;
+// }
+
+//Qno: 02
+
+// let b = prompt('enter number');
+
+// switch (true) {
+//     case (b % 3 == 0):
+//         console.log("number is divisible by 3");
+//         break;
+//     case (b % 3 !== 0):
+//         console.log("number is not divisible by 3")   
+//         break;  
+// }
+
+//Qno: 03
+
+// let result;
+
+// const operator = prompt('Enter operator ( either +, -, * or / ): ');
+
+// const number1 = parseFloat(prompt('Enter first number: '));
+// const number2 = parseFloat(prompt('Enter second number: '));
+
+// switch(operator) {
+//     case '+':
+//          result = number1 + number2;
+//         console.log(`${number1} + ${number2} = ${result}`);
+//         break;
+
+//     case '-':
+//          result = number1 - number2;
+//         console.log(`${number1} - ${number2} = ${result}`);
+//         break;
+
+//     case '*':
+//          result = number1 * number2;
+//         console.log(`${number1} * ${number2} = ${result}`);
+//         break;
+
+//     case '/':
+//          result = number1 / number2;
+//         console.log(`${number1} / ${number2} = ${result}`);
+//         break;
+
+//     default:
+//         console.log('Invalid operator');
+//         break;
+// }
+
+//Qno: 04
+
+// var date = new Date().getDay();
+// console.log(date)
+
+// switch(date){
+//     case 0:
+//     console.log("Sunday");
+//     break;
+    
+//     case 1:
+//     console.log("Monday");
+//     break;
+
+//     case 2:
+//      console.log("Tuesday");
+//     break;
+
+//     case 3:
+//      console.log("wednesday");
+//     break;
+
+//     case 4:
+//      console.log("Thursday");
+//     break;  
+
+//     case 5:
+//     console.log("Friday");
+//     break;
+
+//     case 6:
+//       console.log("Saturday");
+//      break;
+// }
+
+//Qno: 05
+
+// var date = new Date().getDay();
+// console.log(date)
+
+// switch(date){
+//     case 0:
+//     console.log("Sunday");
+//     break;
+    
+//     case 2:
+//     console.log("sturday");
+//     break;
+
+//     default:
+//     console.log("today is Thursday")
+// }
+
+//Qno: 06
+
+// function checkCar() {
+//     var text;
+//     var favCar = prompt("What is your favorite car?");
+//     switch(favCar) {
+//     case "BMW".toLocaleLowerCase():
+//         console.log(text = "German car");
+//         break;
+
+        
+//     case "Ford".toLocaleLowerCase():
+//         console.log(text = "American car");
+//         break;
+
+        
+//     case "Prugeot".toLocaleLowerCase():
+//         console.log(text = "French car");
+//         break;
+
+//         default:
+//         console.log(text = "unkown car");
+//         break;
+//     }
+//     document.write( text);
+//    }
+//    checkCar();
+
+//Qno: 07
+
+// function checkFruit() {
+//     var text;
+//     var fruits = prompt("Enter a fruit name");
+//     switch(fruits) {
+//     case "Banana":
+//         text = "Banana is good!"; 
+//      break;
+//     case "Orange":
+//     text = "I am not a fan of orange.";
+//     break; 
+//     case "Apple":
+//     text = "How you like them apples?";
+//     break; 
+//     default:
+//     text = "I have never heard of that fruit.";
+//     break;
+//     }
+//     document.write(text);
+//    }
+//    checkFruit();
+
+//Qno: 08
+
